a8541061b8903c20b214c11366e2a4e15ea553c5
Carloren <carloren.1996@gmail.com>
2025-11-10 15:41:47 +0100

    Controlar mejor el comporamiento en locuciones

--- frontend/src/components/DubCard.jsx
+++ frontend/src/components/DubCard.jsx
@@ -13,14 +13,14 @@ function DubCard({ item }) {
             ></iframe>
           </div>
         ) : item.image ? (
-          <div className="card-img-top">
+          <div className={item.category === "Locuciones" ? "card-img-top card-img-top-brand" : "card-img-top"}>
             <img className="card-image" src={item.image} alt={item.title} />
           </div>
         ) : null}
         <div className="card-body">
           <h5 className="card-title">{item.title}</h5>
           <p className="card-text">
-            <i>{item.mainCharacter ? item.mainCharacter : "Voces adicionales"}</i><br />
+            <i>{item.mainCharacter ? item.mainCharacter : item.category === "Locuciones" ? "Corporativo" : "Voces adicionales"}</i><br />
             <span className="badge bg-info mt-1">{item.year}</span>
             <span className="badge bg-secondary ms-1">{item.category}</span>
           </p>
